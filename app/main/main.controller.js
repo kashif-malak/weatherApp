65c4f088ba74f57ab0593938219e1fabd888a8ff
Kashif Malak <kashifmalak@yahoo.com>
2016-10-18 15:15:28 +0300

    new files added

---
+++ app/main/main.controller.js
@@ -0,0 +1,48 @@
+
+(function () {
+    'use strict';
+
+    angular.module('geoWeather.main', [])
+    .controller('MainCtrl', 
+                function ($scope, GeolocationService, $window, $state, WeatherjsonpSvc) {
+
+                    $scope.geolocNotSupported = false
+                    $scope.geolocPermDenied = false;
+                    $scope.position = null;
+                    $scope.message = '';
+                    $scope.searchResult = {};// used for testing purpose only
+
+                    if ( $window.navigator) {   /* geolocation available  */
+
+                        GeolocationService().then(function (position) {
+                            $state.go('Weather', { lon: position.lon,lat:position.lat,searchby:'coords'});
+                        }, function (reason) {
+                            $scope.geolocPermDenied = true;
+                        });
+
+                    } else {
+                        $scope.geolocNotSupported = true;  /* geolocation IS NOT available */
+                    }
+
+                    $scope.searchHandler = function (searchTerm) {
+
+                        if (isNaN(searchTerm)) {
+                            WeatherjsonpSvc.findByCityName(searchTerm)
+                                                .then(success, error);
+                        } else {
+                            WeatherjsonpSvc.findByCityCode(searchTerm)
+                                              .then(success, error);
+                        };
+                    };
+
+                    function success(data) {
+                        $scope.searchResult = data.coord;
+                        $state.go("Weather", { lon: data.coord.lon, lat: data.coord.lat, searchby: 'city' });
+                    };
+                    function error(data) {
+                        alert('Error in locating your place');
+                    };
+
+
+                });
+})();
---
+++ app/tests/controllers/main.controller-spec.js
@@ -0,0 +1,35 @@
+﻿
+describe('Test Main Controller', function () {
+    var scope, ctrl, httpBackend, appConstants, serviceEndPoint;
+
+    beforeEach(module("geoWeather"));
+    beforeEach(module("geoWeather.main"));
+
+    beforeEach(
+      inject(
+        function ($controller, $rootScope, WeatherjsonpSvc, $httpBackend, $templateCache) {
+            httpBackend = $httpBackend;
+            $templateCache.put('main/main.html', 'main.html');
+            $templateCache.put('weatherpage/weather-page.html', 'weather-page.html');
+            scope = $rootScope.$new();
+            serviceEndPoint = "http://api.openweathermap.org/data/2.5/weather?APPID=66a3d0941e614b3050f05912a908b65b &q=LONDON&units=metric&callback=JSON_CALLBACK";
+            ctrl = $controller("MainCtrl", {
+                $scope: scope, WeatherjsonpSvc: WeatherjsonpSvc
+            });
+
+            var url = serviceEndPoint +
+            httpBackend.whenJSONP(url).respond({ coord: { lat: 50 } });
+        }
+      )
+    );
+
+    it('should test searchHander method', function () {
+            scope.searchHandler("LONDON");
+            httpBackend.flush();
+          
+                expect().toBe({ coord: { lat: 50 } });
+            
+
+    });
+
+});
